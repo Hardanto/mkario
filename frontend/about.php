18cbdffd7b92f845592341f31a97f42b077b54de
Mitch <mitchkarioredjo@gmail.com>
2024-06-10 12:01:53 +0200

    Major changes to files and paths

--- frontend/about.php
+++ frontend/about.php
@@ -18,18 +18,18 @@ require_once('../vendor/autoload.php');
         <meta content='Portfolio' name='keywords'/>
         
         
-        <link rel='stylesheet' href='frontend/css/style.css'/>
+        <link rel='stylesheet' href='frontend/styling/css/style.css'/>
         
+		
 		<!-- Bootstrap -->
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<!-- 		
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
-        crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
-		integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-         -->
+		<!-- 
+			Include Bootstrap's CSS and JS files dynamically
+			So we can update it once in one file, but will update in all
+			pages that are using it
+		-->
+		
+		<?= include("includes/bootstrap.cdn.php") ?>
+
         <!--
         <link rel='stylesheet' href='frontend/css/landing-style.css'/>
         <link rel="stylesheet" href="frontend/css/fontawesome5/fontawesome/all.css">
