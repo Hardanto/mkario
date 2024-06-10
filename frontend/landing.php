d6c4dcedb96847f47e1dcbfcaf71fea5ed633fa5
Mitch <mitchkarioredjo@gmail.com>
2024-06-10 13:59:57 +0200

    Styled the landing page

--- frontend/landing.php
+++ frontend/landing.php
@@ -101,16 +101,16 @@ $test = new \Required\DatabasePreparations();
 		?>
 
 			<div class="image-container-one">
-
+				<div class="image-layer"></div>
 				<div class='container container-one'>
 					
 					<div class='row'>
 						<div class='col'></div>
 						<div class='col-12 col-lg-6'>
-							<div class='article-container'>
+							<div class='article-container text-center'>
 								<article>
-									<h1 class='fw-light' style='text-align:center;'>Welcome to my Portfolio</h1>
-									<p class='text-center'>
+									<h1 class='fw-light'>Welcome to my Portfolio</h1>
+									<p>
 										This  part of the website is made in Linux, Apache2, MySQL and PHP, also backed by React.js and with Bootstrap for more dynamics. For now I'm only working on the User Interface of it, so no functional backend. But that is yet to come.
 									</p>
 								</article>
@@ -130,8 +130,9 @@ $test = new \Required\DatabasePreparations();
 						
 						<div class='col-12 col-lg-6'>
 							
-							<div class="article-container">
+							<div class="article-container text-center">
 								<article>
+									<h2 class="fw-light">Lorem ipsum dolor sit amet</h2>
 									<p>
 									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 									</p>
@@ -146,12 +147,14 @@ $test = new \Required\DatabasePreparations();
 			</div>
 			
 			<div class="image-container-three">
+			<div class="image-layer"></div>
 				<div class='container container-three'>
 					<div class='row'>
 						<div class='col'></div>
 						<div class='col-12 col-lg-6'>
-							<div class="article-container">
+							<div class="article-container text-center">
 								<article>
+									<h2 class="fw-light">Sed ut perspiciatis</h2>
 									<p>
 									Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 									</p>
