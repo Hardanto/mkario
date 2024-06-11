5bdd1b8086e07fcb04ceb1437f9e3a90b7537a63
Mitch <mitchkarioredjo@gmail.com>
2024-06-11 23:25:20 +0200

    Re-styled and recreated the footer

--- frontend/includes/footer.php
+++ frontend/includes/footer.php
@@ -1,26 +1,21 @@
 <div class='footer-container'>
 	<div class='container'>
 		<div class="row">
-			<div class="col">
-				<h4 class="fw-light">Column one</h4>
-				<ul>
-					<li>
-						<a href="#">Link 1</a>
-					</li>
-					<li>
-						<a href="#">Link 2</a>
-					</li>
-					<li>
-						<a href="#">Link 3</a>
-					</li>
-					<li>
-						<a href="#">Link 4</a>
-					</li>
-				</ul>
+			<div class="col"></div>
+			<div class="col"><h3>&nbsp;</h3></div>
+			<div class="col"></div>
+		</div>
+		<div class="row">
+			<div class="col-lg-1">
+				
 			</div>
-			<div class="col">
+			<div class="col-12 col-lg-5 py-4 px-2">
+				<h4 class="fw-light">Like my Portfolio Website?</h4>
+				
+			</div>
+			<div class="col-6 col-lg-2 py-4 px-2">
 				<h4 class="fw-light">Column two</h4>
-				<ul>
+				<ul class="footer-menus">
 					<li>
 						<a href="#">Link 1</a>
 					</li>
@@ -35,9 +30,9 @@
 					</li>
 				</ul>
 			</div>
-			<div class="col">
+			<div class="col-6 col-lg-2 py-4 px-2">
 				<h4 class="fw-light">Column three</h4>
-				<ul>
+				<ul class="footer-menus">
 					<li>
 						<a href="#">Link 1</a>
 					</li>
@@ -52,9 +47,9 @@
 					</li>
 				</ul>
 			</div>
-			<div class="col">
+			<div class="col-6 col-lg-2 py-4 px-2">
 				<h4 class="fw-light">Column four</h4>
-				<ul>
+				<ul class="footer-menus">
 					<li>
 						<a href="#">Link 1</a>
 					</li>
@@ -80,8 +75,8 @@
 				</a>
 			</div>
 			<div class='col-12 col-lg-6'>&nbsp;</div>
-			<div class='col'>
-				<p>©2024 Mkario.com. All rights reserved.</p>
+			<div class='col copyright'>
+				<p>&copy;2024 Mkario.com. All rights reserved.</p>
 			</div>
 		</div>
 	</div>
