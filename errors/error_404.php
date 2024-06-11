dafbd825b5ffe48f5a1551b8ee0e427f6cdd3e83
Mitch <mitchkarioredjo@gmail.com>
2024-06-11 21:20:17 +0200

    Add styling to the error page

--- errors/error_404.php
+++ errors/error_404.php
@@ -17,12 +17,12 @@ require_once('../vendor/autoload.php');
         <meta content='' name='description'/>
         <meta content='' name='keywords'/>
         
-        <link rel='stylesheet' href='frontend/css/style.css'/>
+        <link rel='stylesheet' href='frontend/styling/css/style.css'/>
 		
 		<!-- Bootstrap -->
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-		
+		<?php include("../frontend/includes/bootstrap.cdn.php"); ?>
+
+
     </head>
 
 	<body class="error e404">
